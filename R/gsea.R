aa9dc6dd309d58c65894554a3c5bb119eb4fa7c6
pwwang <pwwang@pwwang.com>
2025-07-26 21:53:35 -0700

    feat: enhance VizGSEA function with new plotting options (dot and heatmap)
    - Added 'heatmap' and 'dot' as plot types in VizGSEA
    - Introduced parameters: group_by, values_by, signif_by, and signif_cutoff for improved visualization
    - Updated documentation to reflect new parameters and usage

--- R/gsea.R
+++ R/gsea.R
@@ -155,13 +155,26 @@ RunGSEA = function(ranks, genesets, ...) {
 #' @param gsea_results A data frame with the results of the fgsea analysis
 #' @param plot_type The type of plot to create
 #' One of "summary", "gsea"
+#' @param group_by The column name to group by for heatmap and dot plot.
+#' They will be used as the columns in the heatmap or dot plot.
+#' @param values_by The column name to use for the values in the heatmap or dot plot.
+#' Default is "NES" (normalized enrichment score).
+#' @param signif_by The column name to use for significance in the heatmap or dot plot.
+#' Default is "padj" (adjusted p-value). It can also be "pval".
+#' If NULL, no significance labels will be added to the heatmap.
+#' @param signif_cutoff A numeric vector of significance cutoffs for the heatmap labels.
+#' Multiple values can be provided to indicate different levels (at most 3) of significance.
+#' For example, `c(0.05, 0.01, 0.001)` will label pathways with p-values less than 0.05 with "*",
+#' less than 0.01 with "**", and less than 0.001 with "***".
 #' @inheritParams plotthis::GSEASummaryPlot
 #' @inheritParams plotthis::GSEAPlot
 #' @param ... Additional arguments passed to the plotting function
 #' * When `plot_type` is "summary", they are passed to [plotthis::GSEASummaryPlot()]
 #' * When `plot_type` is "gsea", they are passed to [plotthis::GSEAPlot()]
 #' @return A ggplot object or a list of ggplot objects
-#' @importFrom plotthis GSEASummaryPlot GSEAPlot
+#' @importFrom rlang sym parse_expr
+#' @importFrom dplyr mutate %>%
+#' @importFrom plotthis GSEASummaryPlot GSEAPlot Heatmap DotPlot
 #' @export
 #' @examples
 #' \donttest{
@@ -188,16 +201,97 @@ RunGSEA = function(ranks, genesets, ...) {
 #' # Visualize the GSEA results
 #' VizGSEA(r, plot_type = "summary")
 #' VizGSEA(r, plot_type = "gsea", gs = c("set10", "set2"))
+#'
+#' r$Group <- "A"
+#' r2 <- r
+#' r2$Group <- "B"
+#' r2$NES <- sample(r2$NES)
+#' VizGSEA(rbind(r, r2), plot_type = "heatmap")
 #' }
 VizGSEA <- function(
-    gsea_results, plot_type = c("summary", "gsea"),
-    gene_ranks = "@gene_ranks", gene_sets = "@gene_sets", gs = NULL, ...)
-{
+    gsea_results, plot_type = c("summary", "gsea", "heatmap", "dot"),
+    gene_ranks = "@gene_ranks", gene_sets = "@gene_sets", gs = NULL,
+    group_by = NULL, values_by = "NES", signif_by = "padj", signif_cutoff = 0.05,
+    ...
+) {
     plottype <- match.arg(plot_type)
 
     if (plot_type == "summary") {
         GSEASummaryPlot(gsea_results, gene_ranks = gene_ranks, gene_sets = gene_sets, ...)
     } else if (plot_type == "gsea") {
         GSEAPlot(gsea_results, gene_ranks = gene_ranks, gene_sets = gene_sets, gs = gs, ...)
+    } else if (plot_type == "heatmap") {
+        stopifnot("[VizGSEA] 'group_by' is required for heatmap" = !is.null(group_by))
+        if (!is.null(signif_by)) {
+            cell_type = "label"
+            signif_df <- gsea_results[, c("pathway", group_by, signif_by), drop = FALSE] %>%
+                tidyr::pivot_wider(
+                    names_from = group_by,
+                    values_from = signif_by
+                ) %>%
+                as.data.frame()
+            rownames(signif_df) <- signif_df$pathway
+            signif_df$pathway <- NULL
+            signif_df <- as.matrix(signif_df)
+            signif_cutoff <- sort(signif_cutoff)
+            if (length(signif_cutoff) > 3) {
+                stop("[VizGSEA] 'signif_cutoff' should have at most 3 values")
+            }
+            label <- function(x, i, j, rn, cn) {
+                # don't use i, j here, use rn, cn instead, in case
+                # the rownames and colnames of signif_df is different
+                # from the rownames and colnames of the one passed to ComplexHeatmap::Heatmap()
+                idx_i <- match(rn, rownames(signif_df))
+                idx_j <- match(cn, colnames(signif_df))
+                sig <- ComplexHeatmap::pindex(signif_df, idx_i, idx_j)
+                if (length(signif_cutoff) == 1) {
+                    ifelse(sig < signif_cutoff, "*", NA)
+                } else if (length(signif_cutoff) == 2) {
+                    ifelse(sig < signif_cutoff[1], "**",
+                        ifelse(sig < signif_cutoff[2], "*", NA)
+                    )
+                } else if (length(signif_cutoff) == 3) {
+                    ifelse(sig < signif_cutoff[1], "***",
+                        ifelse(sig < signif_cutoff[2], "**",
+                            ifelse(sig < signif_cutoff[3], "*", NA)
+                        )
+                    )
+                }
+            }
+        } else {
+            cell_type = "tile"
+            label <- NULL
+        }
+        Heatmap(
+            gsea_results,
+            in_form = "long",
+            values_by = values_by,
+            rows_by = "pathway",
+            columns_by = group_by,
+            cell_type = cell_type,
+            label = label,
+            ...
+        )
+    } else if (plot_type == "dot") {
+        stopifnot("[VizGSEA] 'group_by' is required for dot plot" = !is.null(group_by))
+        if (signif_by == "padj") {
+            fill_by <- "-log10(padj)"
+        } else if (signif_by == "pval") {
+            fill_by <- "-log10(pval)"
+        } else {
+            fill_by <- signif_by
+        }
+        gsea_results <- mutate(
+            gsea_results,
+            !!sym(fill_by) := !!parse_expr(fill_by)
+        )
+        DotPlot(
+            gsea_results,
+            x = group_by,
+            y = "pathway",
+            fill_by = values_by,
+            size_by = fill_by,
+            ...
+        )
     }
 }
