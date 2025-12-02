f54fba1c6601b6ac12ca05ca90fa22471e0a4274
pwwang <pwwang@pwwang.com>
2025-12-02 07:19:44 -0700

    chore: enhance .onAttach to list available fonts from plotthis package

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,22 @@
 .onAttach <- function(libname, pkgname) {
-    options("theme_this.font_family" = "LiberationSans")
+    fonts <- plotthis::list_fonts(family_only = TRUE)
+    packageStartupMessage(
+        "Listing available fonts from plotthis package:\n",
+        "Builtin fonts:\n",
+        paste(paste0(" - ",
+                     sapply(split(fonts$builtin, ceiling(seq_along(fonts$builtin)/5)),
+                            function(x) paste(x, collapse = ", "))),
+              collapse = "\n"),
+        "\nSystem fonts:\n",
+        paste(paste0(" - ",
+                     sapply(split(fonts$system, ceiling(seq_along(fonts$system)/5)),
+                            function(x) paste(x, collapse = ", "))),
+              collapse = "\n"),
+        "\nGoogle fonts (need internet access):\n",
+        paste(paste0(" - ",
+                     sapply(split(fonts$google, ceiling(seq_along(fonts$google)/5)),
+                            function(x) paste(x, collapse = ", "))),
+              collapse = "\n"),
+        "\n"
+    )
 }
